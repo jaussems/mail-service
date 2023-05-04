6cdeb12e112236b90c4934a24e90e9e194ec9f45
jaussems <jannesaussems@gmail.com>
2023-05-04 21:38:17 +0200

    generated and added mail module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { MailModule } from './mail/mail.module';
       }),
     }),
     MailModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { MailModule } from 'src/mail/mail.module';
+
+@Module({
+  imports: [MailModule], // 📧
+  providers: [AuthService],
+})
+export class AuthModule {}

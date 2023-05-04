d2ec695d9915ceca1642139864519d73aa2afc27
jaussems <jannesaussems@gmail.com>
2023-05-04 23:28:29 +0200

    protected email .env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private _mailService: MailService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(config: ConfigService): any {
+    //return this.appService.getHello();
+    return this._mailService.sendUserConfirmation(
+      {
+        email: config.get('MAIL_USER'),
+        name: 'Jannes',
+      },
+      'fdsfdsfdsffs',
+    );
   }
 }

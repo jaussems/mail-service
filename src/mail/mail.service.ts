d7e06abf9903cda6bbb0e0adc2c03cff63d9dc33
jaussems <jannesaussems@gmail.com>
2023-05-07 17:28:23 +0200

    using outlook instead of gmail, test succeeded

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,17 +1,21 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { User } from 'src/user/user.entity';
 
 @Injectable()
 export class MailService {
-  constructor(private mailerService: MailerService) {}
+  constructor(
+    private mailerService: MailerService,
+    private _configService: ConfigService,
+  ) {}
 
   async sendUserConfirmation(user: User, token: string) {
     const url = `example.com/auth/confirm?token=${token}`;
 
     await this.mailerService.sendMail({
       to: user.email,
-      from: 'support@localhost.com', // override default from
+      from: this._configService.get('MAIL_USER'), // override default from
       subject: 'Welcome to Nice App! Confirm your Email',
       template: './confirmation', // `.hbs` extension is appended automatically
       context: {
